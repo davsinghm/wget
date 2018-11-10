df381fa294b5319ed676554ab31f17e23c430c6e
Davinder Singh <davsinghm@gmail.com>
2018-11-10 20:58:58 +0000

    cosmetic: rearrange code

--- src/main/java/com/davsinghm/wget/core/Direct.java
+++ src/main/java/com/davsinghm/wget/core/Direct.java
@@ -9,10 +9,10 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public abstract class Direct {
 
+    public static final int BUF_SIZE = 4 * 1024; // size of read buffer
     protected Context context;
     private File target;
     private DownloadInfo info;
-    public static final int BUF_SIZE = 4 * 1024; // size of read buffer
 
     public Direct(Context context, DownloadInfo info, File target) {
         this.context = context;
