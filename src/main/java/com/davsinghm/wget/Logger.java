fc6389014d0296abbc483a50482f938d513842b6
Davinder Singh <davsinghm@gmail.com>
2019-01-23 12:48:49 +0000

    logs: simplify logger functions

--- src/main/java/com/davsinghm/wget/Logger.java
+++ src/main/java/com/davsinghm/wget/Logger.java
@@ -28,16 +28,6 @@ public class Logger {
             callback.wtf(tag, t);
     }
 
-    public static void wtf(String tag, String msg) {
-        if (callback != null)
-            callback.wtf(tag, msg);
-    }
-
-    public static void wtf(String tag, String msg, Throwable t) {
-        if (callback != null)
-            callback.wtf(tag, msg, t);
-    }
-
     public static void d(String tag, String msg) {
         if (callback != null)
             callback.d(tag, msg);
@@ -48,48 +38,21 @@ public class Logger {
             callback.e(tag, msg);
     }
 
-    public static void i(String tag, String msg) {
-        if (callback != null)
-            callback.i(tag, msg);
-    }
-
     public static void w(String tag, String msg) {
         if (callback != null)
             callback.w(tag, msg);
-    }
-
-    public static void w(String tag, Throwable t) {
-        if (callback != null)
-            callback.w(tag, t);
-    }
-
-    public static void w(String tag, String msg, Throwable t) {
-        if (callback != null)
-            callback.w(tag, msg, t);
     }
 
     public interface Callback {
 
         void wtf(String tag, Throwable t);
 
-        void wtf(String tag, String msg);
-
-        void wtf(String tag, String msg, Throwable t);
-
         void d(String tag, String msg);
 
         void e(String tag, String msg);
 
-        void e(String tag, Throwable t);
-
-        void e(String tag, String msg, Throwable t);
-
         void i(String tag, String msg);
 
         void w(String tag, String msg);
-
-        void w(String tag, Throwable t);
-
-        void w(String tag, String msg, Throwable t);
     }
 }
