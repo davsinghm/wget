dfba83f1c8109197ff743a3a878ece19ec07cfea
Davinder Singh <davsinghm@gmail.com>
2019-01-23 10:50:18 +0000

    utils: remove unneeded function

--- src/main/java/com/davsinghm/wget/core/util/StringUtil.java
+++ src/main/java/com/davsinghm/wget/core/util/StringUtil.java
@@ -15,16 +15,4 @@ public class StringUtil {
         }
         return result.toString();
     }
-
-    public static String join(String prefix, String delimiter, List<String> elements) {
-
-        StringBuilder result = new StringBuilder();
-        int i = 0;
-        for (String element : elements) {
-            result.append(element);
-            if (++i != elements.size())
-                result.append(delimiter);
-        }
-        return result.toString();
-    }
 }
