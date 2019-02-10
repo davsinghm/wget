76b407f16e86ee287305472b1b2681f19e297143
Davinder Singh <davsinghm@gmail.com>
2019-02-10 19:05:11 +0000

    refactor/cosmetic: remove unused imports

--- src/main/java/com/davsinghm/wget/core/info/DownloadInfo.java
+++ src/main/java/com/davsinghm/wget/core/info/DownloadInfo.java
@@ -9,7 +9,6 @@ import com.davsinghm.wget.core.SpeedInfo;
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
